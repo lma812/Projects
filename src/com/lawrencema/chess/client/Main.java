30c20d3f85286c8f00c7ee639d8a6586d43f9a09
lma812 <lawrence.ma16@bcmail.cuny.edu>
2025-04-21 01:04:16 -0400

    Project Structure

---
+++ src/com/lawrencema/chess/client/Main.java
@@ -0,0 +1,8 @@
+package com.lawrencema.chess.client;
+
+public class Main {
+    public static void main(String[] args){
+
+
+    }
+}
---
+++ src/com/lawrencema/chess/server/Main.java
@@ -0,0 +1,8 @@
+package com.lawrencema.chess.server;
+
+public class Main {
+    public static void main(String[] args){
+
+
+    }
+}
